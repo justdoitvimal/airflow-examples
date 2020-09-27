64c6a16d593b3e633a505414f0b38b22c8810f6f
Vimal Yadav <justdoitvimal@gmail.com>
2020-09-27 22:11:38 +0530

    Airflow running code examples

---
+++ airflow_examples/example/dags/example_python_operator.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+#Import statements
+
+from __future__ import print_function
+
+import time
+from datetime import datetime
+from builtins import range
+from pprint import pprint
+
+from airflow.utils.dates import days_ago
+
+from airflow.models import DAG
+from airflow.operators.python_operator import PythonOperator
+
+default_args = {
+	'owner':'vimal',
+	'start_date': days_ago(2)
+}
+	
+dag=DAG(
+	dag_id='example_python_operator',
+	default_args=default_args,
+	schedule_interval="@once",
+	)
+	
+def print_content(ds,**kwargs):
+	
+	dateTimeObj = datetime.now()
+	print('timestamp is ',dateTimeObj)
+	pprint(kwargs)
+	print(ds)
+	return "Heyyyyyyyyyyyyyy I passed"
+
+	
+task1=PythonOperator(
+	task_id= "print_the_content",
+	provide_context=True,
+	python_callable=print_content,
+	dag=dag,
+	)
+	
+def my_Sleeping_funciton(random_base):
+	
+	time.sleep(random_base)
+	
+for i in range(5):
+	task=PythonOperator(
+		task_id='sleep_for_' + str(i),
+		python_callable=my_Sleeping_funciton,
+		op_kwargs={'random_base': float(i)/10},
+		dag=dag,
+		
+		)
+		
+	task1>>task
+	
+	
+	
